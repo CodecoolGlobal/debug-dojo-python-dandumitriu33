febc19f8f820a47210c5a1e839072baa58a62730
dandumitriu33 <dandumitriu33@gmail.com>
2019-11-14 11:36:38 +0200

    Fix the debug.py code

--- debug.py
+++ debug.py
@@ -1,7 +1,7 @@
 import os
 
 
-def filetree(root, path="", exep=[".py"]):
+def filetree(root, path="", exep=[".py", '.md']):
 
     # Stop if this is not a folder:
     if not os.path.isdir(root):
@@ -10,12 +10,12 @@ def filetree(root, path="", exep=[".py"]):
     # Print root name if this is the first call:
     if path == "":
         print("")
-        print(os.path.realpath(root).split("/")[0])
+        print(os.path.realpath(root).split("/")[-1])
 
     # Get files in current folder:
     files = sorted(os.listdir(root))
     files_filtered = [item for item in files if (not item.startswith(".") and
-                                                 not os.path.splitext(item)[1] in path)]
+                                                 not os.path.splitext(item)[1] in exep)]
     nfiles = len(files_filtered)
 
     # Print the content of the current folder:
@@ -24,10 +24,10 @@ def filetree(root, path="", exep=[".py"]):
     for i in range(nfiles):
         if i == nfiles - 1:
             newpath = path + "    "
-            arrow = "`---- "
+            arrow = "`-- "
         print("{:s}{:s}{:s}".format(path, arrow, files_filtered[i]))
         # Recursive call to print the content of sub-folders:
-        filetree("{:s}/{:s}".format(root, files_filtered[i-1]), newpath, exep)
+        filetree("{:s}/{:s}".format(root, files_filtered[i]), newpath, exep)
 
 
 def main():
